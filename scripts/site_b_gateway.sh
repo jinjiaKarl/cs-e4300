27dab0c5132b9b86ab24b6e9ce8d14fb0e5f6154
Hollow Man <hollowman@opensuse.org>
2022-12-07 00:52:19 +0200

    fix: enable debug
    Signed-off-by: Hollow Man <hollowman@opensuse.org>

--- scripts/site_b_gateway.sh
+++ scripts/site_b_gateway.sh
@@ -150,7 +150,7 @@ conn b-to-cloud
         ike=aes256gcm16-prfsha384-ecp384!
         esp=aes256gcm16-ecp384!
         auto=start
-        dpdaction=hold
+        dpdaction=restart
 EOL
 
 ## Restart ipsec for updates to take effect
